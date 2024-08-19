f2af2147efd7c26c21fc8c2d9384351c2ac56601
Pradyumna Kombethota Ramgopal <pradyumnahebbar552@gmail.com>
2024-08-18 20:36:52 -0400

    first commit

---
+++ detector.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 22:32:55 2018
+
+@author: vipul tushar pradyumna
+"""
+
+import cv2
+faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cam = cv2.VideoCapture(0)
+rec = cv2.face.LBPHFaceRecognizer_create()
+rec.read("recognizer/trainingData.yml")
+id = 0
+fontface = cv2.FONT_HERSHEY_SIMPLEX
+fontsize = 1
+fontcolor = (0,511,1)
+while(True):
+    ret,img = cam.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceDetect.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,180),2)
+        id,conf = rec.predict(gray[y:y+h,x:x+w])
+        if(id==1):
+               id="Pradyumna"
+        elif(id==2):
+               id="Tom Hanks"
+        elif(id==3):
+               id="Vipul"
+        cv2.putText(img,str(id),(x,y+h+25),fontface,fontsize,fontcolor,2)
+    cv2.imshow("Face",img)
+    if(cv2.waitKey(1)==ord('q')):
+        break
+cam.release()
+cv2.destroyAllWindows()
